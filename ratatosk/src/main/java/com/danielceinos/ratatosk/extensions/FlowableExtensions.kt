dfa8b18b9df6f11ddbf1c853489e87a55afa959e
Daniel Sanchez <daniel.ceinos@gmail.com>
2019-06-02 21:45:54 +0200

    Added Utils rx extensions

--- ratatosk/src/main/java/com/danielceinos/ratatosk/extensions/FlowableExtensions.kt
+++ ratatosk/src/main/java/com/danielceinos/ratatosk/extensions/FlowableExtensions.kt
@@ -1,26 +1,39 @@
 package com.danielceinos.ratatosk.extensions
 
 import io.reactivex.Flowable
+import io.reactivex.Observable
+import mini.mapNotNull
 
 /**
- * Apply the mapping function if object is not null and if is null return the default value.
+ * Apply the mapping function if object is not null.
  */
-inline fun <T, U> Flowable<T>.mapNull(
-    crossinline fn: (T) -> U?,
-    default: U
-): Flowable<U> {
+inline fun <T, U> Flowable<T>.mapNotNull(crossinline fn: (T) -> U?): Flowable<U> {
     return flatMap {
         val mapped = fn(it)
-        if (mapped == null) Flowable.just(default)
+        if (mapped == null) Flowable.empty()
         else Flowable.just(mapped)
     }
 }
 
 /**
- * Apply the mapping function if object is not null or return default value if null, together with a distinctUntilChanged call.
+ * Apply the mapping function if object is not null.
  */
-inline fun <T, U> Flowable<T>.select(
-    crossinline fn: (T) -> U?,
-    default: U
-): Flowable<U> =
-    mapNull(fn, default).distinctUntilChanged()
+inline fun <T, U> Observable<T>.mapNotNull(crossinline fn: (T) -> U?): Observable<U> {
+    return flatMap {
+        val mapped = fn(it)
+        if (mapped == null) Observable.empty()
+        else Observable.just(mapped)
+    }
+}
+
+/**
+ * Apply the mapping function if object is not null together with a distinctUntilChanged call.
+ */
+inline fun <T, U> Flowable<T>.select(crossinline fn: (T) -> U?): Flowable<U> =
+    mapNotNull(fn).distinctUntilChanged()
+
+/**
+ * Apply the mapping function if object is not null together with a distinctUntilChanged call.
+ */
+inline fun <T, U> Observable<T>.select(crossinline fn: (T) -> U?): Observable<U> =
+    mapNotNull(fn).distinctUntilChanged()
